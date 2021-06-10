d39d4857d3856478e3a9ee80490b48cea716e64f
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2021-06-10 12:02:11 -0300

    Add files via upload

---
+++ atividade avaliativa 5 L(5).c
@@ -0,0 +1,26 @@
+/*5. Faça um programa que pergunte ao usuário qual a letra inicial do seu sexo (masculino/feminino) e
+escreva por extenso o sexo informado, usando o operador condicional ternário.*/
+
+#include<stdio.h>
+#include<locale.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	char sexo;
+	
+	printf("Digite a letra inicial do seu sexo |M| - masculino |F| - feminino:");
+	scanf("%c", &sexo);
+	fflush(stdin);
+	
+	sexo = toupper(sexo);
+	
+	system("cls");
+	
+	sexo == 'M' ? printf("Seu sexo é masculino.\n") : printf("Seu sexo é feminino.\n");
+		
+	system("pause");
+    return 0;
+}
