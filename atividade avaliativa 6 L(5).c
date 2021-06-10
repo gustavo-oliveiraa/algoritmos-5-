db373413d5ce06a4d68de64fdd4770811d8d2ba0
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2021-06-10 00:11:12 -0300

    Add files via upload

--- atividade avaliativa 6 L(5).c
+++ atividade avaliativa 6 L(5).c
@@ -1,15 +1,15 @@
-/*6. Desenvolva um programa em C que apresente um menu de opÃ§Ãµes numa janela â€œbemâ€ amigÃ¡vel ao
-usuÃ¡rio, onde este deverÃ¡ escolher qual a regiÃ£o do Brasil que ele gostaria de conhecer. ApÃ³s o usuÃ¡rio
-informar esta regiÃ£o, limpe toda a janela e escreva no meio dela qual foi a opÃ§Ã£o do usuÃ¡rio. Lembrando
-que as regiÃµes deverÃ£o ser apresentadas conforme o seguinte menu de opÃ§Ãµes:
-Menu de OpÃ§Ãµes
+/*6. Desenvolva um programa em C que apresente um menu de opções numa janela “bem” amigável ao
+usuário, onde este deverá escolher qual a região do Brasil que ele gostaria de conhecer. Após o usuário
+informar esta região, limpe toda a janela e escreva no meio dela qual foi a opção do usuário. Lembrando
+que as regiões deverão ser apresentadas conforme o seguinte menu de opções:
+Menu de Opções
 ============
-1 â€“ Norte
-2 â€“ Nordeste
-3 â€“ Centro-Oeste
-4 â€“ Sudeste
-5 â€“ Sul
-0 â€“ Sair do programa*/
+1 – Norte
+2 – Nordeste
+3 – Centro-Oeste
+4 – Sudeste
+5 – Sul
+0 – Sair do programa*/
 
 #include<stdio.h>
 #include<locale.h>
@@ -21,40 +21,40 @@ int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int numero;
 	
-	printf("Menu de OpÃ§Ãµes:\n");
+	printf("Menu de Opções:\n");
 	printf("=============\n");
-	printf("1 â€“ Norte.\n");
-	printf("2 â€“ Nordeste.\n");
-	printf("3 â€“ Centro-Oeste.\n");
-	printf("4 â€“ Sudeste.\n");
-	printf("5 â€“ Sul.\n");
-	printf("0 â€“ Sair do programa.\n");
+	printf("1 – Norte.\n");
+	printf("2 – Nordeste.\n");
+	printf("3 – Centro-Oeste.\n");
+	printf("4 – Sudeste.\n");
+	printf("5 – Sul.\n");
+	printf("0 – Sair do programa.\n");
 
 	do{
 	
-		printf("\nDigite a nÃºmeraÃ§Ã£o da regiÃ£o que deseja conhecer:");
+		printf("\nDigite a númeração da região que deseja conhecer:");
 		scanf("%d", &numero);
 		fflush(stdin);
 	
 		system("cls");
 	
 		if(numero == 1)
-			printf("Sua opÃ§Ã£o foi a 1, regiÃ£o Norte!\n");
+			printf("\t \t \t \t \tSua opção foi a 1, região Norte!\n");
 				else if(numero == 2)
-					printf("Sua opÃ§Ã£o foi a 2, regiÃ£o Nordeste!\n");
+					printf("\t \t \t \t \tSua opção foi a 2, região Nordeste!\n");
 						else if(numero == 3)
-							printf("Sua opÃ§Ã£o foi a 3, regiÃ£o Centro-Oeste!\n");
+							printf("\t \t \t \t \tSua opção foi a 3, região Centro-Oeste!\n");
 								else if(numero == 4)
-									printf("Sua opÃ§Ã£o foi a 4, regiÃ£o Sudeste!\n");
+									printf("\t \t \t \t \tSua opção foi a 4, região Sudeste!\n");
 										else if(numero == 5)
-											printf("Sua opÃ§Ã£o foi a 5, regiÃ£o Sul!\n");
+											printf("\t \t \t \t \tSua opção foi a 5, região Sul!\n");
 												else if(numero==0)
-													printf("Saindo do programa...\n");
+													printf("\t \t \t \t \tSaindo do programa...\n");
 														else
-	 														printf("NÃºmero nÃ£o encontrado, no programa.\n");
+	 														printf("\t \t \t \t \tNúmero não encontrado, no programa.\n");
 	
 	}while(numero!=0);
 	
-    system("pause");
+	system("pause");
     return 0;
 }
